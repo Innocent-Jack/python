2e583c1d13471949ccd13d94134577d6d074c2e4
Innocent-Jack <jacksonraj4726@gmail.com>
2026-07-14 16:09:09 +0530

    Add files via upload

---
+++ Jack3.py
@@ -0,0 +1,10 @@
+# Program to find the sum of first n natural numbers
+
+n = int(input("Enter a positive integer: "))
+
+sum = 0
+
+for i in range(1, n + 1):
+    sum += i
+
+print("The sum of the first", n, "natural numbers is:", sum)
